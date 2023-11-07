85b61e569f8628fc86138639890adcb94fd656ae
Matthew Amosa <amosa.matthew@pm.me>
2023-11-07 05:10:24 -0700

    initial commit

---
+++ data/hotels.js
@@ -0,0 +1,50 @@
+export const hotels = [
+  {
+    "name": "Salt Lake City",
+    "photo": "1.webp",
+    "address": ["555 South Main Street","Salt Lake City, UT 84111-4100"],
+    "phone": "+1-801-258-6000"
+  },
+  {
+    "name": "Kailua-Kona",
+    "photo": "2.webp",
+    "address": ["75-5852 Alii Drive","Kailua-Kona, Island of Hawaii, HI 96740-1310"],
+    "phone": "+1-808-353-5931"
+  },
+  {
+    "name": "Cabo San Lucas",
+    "photo": "3.webp",
+    "address": ["Carretera Transpeninsular Km. 17.5 Corredor Turístico","Cabo San Lucas 23405 Mexico"],
+    "phone": "011 52 800 931 6023"
+  },
+  {
+    "name": "Rome",
+    "photo": "4.webp",
+    "address": ["Via Nazionale 22","00184 Rome Italy"],
+    "phone": "011 39 06 489911"
+  },
+  {
+    "name": "Paris",
+    "photo": "5.webp",
+    "address": ["4 Rue de Valois","75001 Paris France"],
+    "phone": "+33 1 42 96 15 35"
+  },
+  {
+    "name": "Tahiti",
+    "photo": "6.webp",
+    "address": ["Arue 98702","French Polynesia"],
+    "phone": "+689 40 86 63 00"
+  },
+  {
+    "name": "Bangkok",
+    "photo": "7.webp",
+    "address": ["372 Sri Ayutthaya Road","Thanon Phyathai Rajthevi, Bangkok 10400 Thailand"],
+    "phone": "+66 (0) 2248 5511-15"
+  },
+  {
+    "name": "St. Petersburg",
+    "photo": "8.webp",
+    "address": ["Voznesenskiy Prospekt, d. 1, St.","Petersburg 190031 Russia"],
+    "phone": "+7 812 339 80 00"
+  }
+]
